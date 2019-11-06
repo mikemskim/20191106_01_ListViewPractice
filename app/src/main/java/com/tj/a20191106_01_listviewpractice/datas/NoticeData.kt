36faf223505cacddf79cef07280951e2f97a6a13
mikemskim <mskim@steconsulting.co.kr>
2019-11-06 19:56:14 +0900

    데이터 클래스 생성

---
+++ app/src/main/java/com/tj/a20191106_01_listviewpractice/datas/NoticeData.kt
@@ -0,0 +1,7 @@
+package com.tj.a20191106_01_listviewpractice.datas
+
+class NoticeData(title:String, content:String, writeDate:String) {
+    var title = title
+    var content = content
+    var writeData = writeDate
+}
